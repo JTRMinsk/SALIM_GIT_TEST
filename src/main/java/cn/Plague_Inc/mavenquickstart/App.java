d314601e5fcd6aa50e7fcd97cae3a707786d1f47
JTRMinsk <jtrkiev@yahoo.com>
2019-10-31 20:31:29 +0800

    F1Project

---
+++ src/main/java/cn/Plague_Inc/mavenquickstart/App.java
@@ -0,0 +1,13 @@
+package cn.Plague_Inc.mavenquickstart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
